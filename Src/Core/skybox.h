c96049ebed7813e62481b99fa67e9ebd73126606
Amarnath <amarnath2105@gmail.com>
2024-01-04 16:40:00 -0700

    Skybox z position fix

--- Src/Core/skybox.h
+++ Src/Core/skybox.h
@@ -11,7 +11,7 @@ class Skybox : public SceneObject
 	private:
 		GLuint VAO = GL_INVALID_INDEX, VBO = GL_INVALID_INDEX;
 		
-		std::vector<float> vertData{ -1.0f,-1.0f,0.999f,
-									-1.0f,4.0f,0.999f,
-									4.0f,-1.0f,0.999f };
+		std::vector<float> vertData{ -1.0f,-1.0f,0.9999f,
+									-1.0f,4.0f,0.9999f,
+									4.0f,-1.0f,0.9999f };
 };
