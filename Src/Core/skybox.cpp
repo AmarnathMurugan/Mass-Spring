8248c0a32b0b5747ea916baf0a786c51607312d9
Amarnath <amarnath2105@gmail.com>
2024-01-03 18:16:05 -0700

    Screen space triangle working

--- Src/Core/skybox.cpp
+++ Src/Core/skybox.cpp
@@ -12,7 +12,7 @@ void Skybox::setBuffers()
 
 	glGenBuffers(1, &this->VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(this->vertData), this->vertData.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * this->vertData.size(), this->vertData.data(), GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
